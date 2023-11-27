d9f4be354b6962cdd1becb871259c9c15f5ebb87
WU-YINGHONG <akashidaiki2@gmail.com>
2023-11-27 23:04:35 +0800

    增加編輯頁圖片顯示

--- edit_file.php
+++ edit_file.php
@@ -1,5 +1,10 @@
 <?php
 include_once 'db.php';
+if (isset($_GET['id'])) {
+  $file = find('files', $_GET['id']);
+} else {
+  exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +14,10 @@ include_once 'db.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>編輯檔案</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+
   <link rel="stylesheet" href="style.css">
 </head>
 
@@ -24,14 +33,54 @@ include_once 'db.php';
 
   ?>
 
-
+  <div class="text-center"><a href="manage.php">回檔案管理頁</a></div>
   <form action="api/edit_file.php" method="post" enctype="multipart/form-data">
+    <div class="container col-6">
 
-    <input type="file" name="img" id="">
-    <input type="text" name="name" id="" value="">
-    <input type="text" name="desc" id="" value="">
-    <input type="submit" value="更新">
+      <table class="table">
+        <tr>
+          <?php
+          switch ($file['type']) {
+            case "image/webp":
+            case "image/jpeg":
+            case "image/png":
+            case "image/gif":
+              $imgname = "./imgs/" . $file['name'];
+              break;
+            case "msword":
+              $imgname = "./icon/wordicon.png";
+              break;
+            case "msexcel":
+              $imgname = "./icon/excelicon.png";
+              break;
+            case "msppt":
+              $imgname = "./icon/ppticon.png";
+              break;
+            case "pdf":
+              $imgname = "./icon/pdficon.png";
+              break;
+            default:
+              $imgname = "./icon/othericon.png";
+          }
+          ?>
+          <td>媒體</td>
+          <img src="<?= $imgname; ?>" alt="" style="width:200px; 200px;">
+          <td><input type=" file" name="img"></td> <!-- file 屬性的 input 沒有 value 這個屬性 -->
+        </tr>
+        <tr>
+          <td>檔名</td>
+          <td><input type="text" name="name" value="<?= $file['name']; ?>"></td>
+        </tr>
+        <tr>
+          <td>說明</td>
+          <td><textarea name="desc" style="width:300px; height:200px;"><?= $file['desc']; ?></textarea></td>
+        </tr>
+      </table>
+      <div class=" text-center m-3">
+        <input type="submit" value="更新">
+      </div>
   </form>
+  </div>
 
 
   <!----建立一個連結來查看上傳後的圖檔---->
@@ -42,6 +91,10 @@ include_once 'db.php';
 
 
   ?>
+  <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+
 </body>
 
 </html>
--- manage.php
+++ manage.php
@@ -80,26 +80,26 @@ include_once "db.php";
           <?= $file['id']; ?>
         </td>
         <!-- 下面這一行的 $file['name'] 跟 imgs 後面的 2023112316302713652.jpg 一樣，所以把它放到 imgs 的後面  -->
-        <td><img style="width:150px;height:120px;" src="<?= $imgname ?>" alt=""></td>
-        <td>
-          <?= $file['type']; ?>
-        </td>
-        <td>
-          <?= $file['size']; ?>
-        </td>
-        <td>
-          <?= $file['desc']; ?>
-        </td>
-        <td>
-          <?= $file['create_at']; ?>
-        </td>
-        <td>
-          <button class="btn btn-primary" onclick="location.href='./edit_file.php?id=<?= $file['id'] ?>'">編輯</button>
-          <button class="btn btn-danger" onclick="location.href='./api/del_file.php?id=<?= $file['id'] ?>'">刪除</button>
-        </td>
-      </tr>
+          <td><img style="width:150px;height:120px;" src="<?= $imgname ?>" alt=""></td>
+          <td>
+            <?= $file['type']; ?>
+          </td>
+          <td>
+            <?= $file['size']; ?>
+          </td>
+          <td>
+            <?= $file['desc']; ?>
+          </td>
+          <td>
+            <?= $file['create_at']; ?>
+          </td>
+          <td>
+            <button class="btn btn-primary" onclick="location.href='./edit_file.php?id=<?= $file['id'] ?>'">編輯</button>
+            <button class="btn btn-danger" onclick="location.href='./api/del_file.php?id=<?= $file['id'] ?>'">刪除</button>
+          </td>
+        </tr>
 
-      <?php
+        <?php
       }
       ?>
     </table>
@@ -108,7 +108,7 @@ include_once "db.php";
 
   <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-  </script>
+    </script>
 </body>
 </body>
 
