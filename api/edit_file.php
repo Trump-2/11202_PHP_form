d43edd1f4e4753362f439a023621f49c67b4b3b5
Trump-2 <akashidaiki2@gmail.com>
2023-11-28 21:06:46 +0800

    完成編輯檔案的功能

--- api/edit_file.php
+++ api/edit_file.php
@@ -1,35 +1,23 @@
 <?php
 include_once '../db.php';
-
+if (isset($_POST['id'])) {
+  $file = find('files', $_POST['id']);
+} else {
+  exit();
+}
 // echo $_POST['desc'];
 
 // 確認檔案是否上傳成功，透過判斷暫存的路徑
 // tmp_name 是完整的路徑，包含檔名
 if (!empty($_FILES['img']['tmp_name'])) {
-  // echo $_FILES['img']['tmp_name'];
-  // echo "<br>";
-  // echo $_FILES['img']['name'];
-  // echo "<br>";
-  // echo $_FILES['img']['type'];
-  // echo "<br>";
-  // echo $_FILES['img']['size'];
+  if ($_POST['name'] != $file['name']) {
+    move_uploaded_file($_FILES['img']['tmp_name'], "../imgs/" . $_POST['name']);
+    $file['name'] = $_POST['name'];
+  } else {
+    move_uploaded_file($_FILES['img']['tmp_name'], "../imgs/" . $_POST['name']);
+  }
 
-  // 這裡第二個參數最好的做法是要修改檔案的原名稱，因為可能產生檔名衝突，但這邊為了方便讓新檔名跟原檔名一樣
-  // move_uploaded_file($_FILES['img']['tmp_name'], "../imgs/" . $_FILES['img']['name']);
 
-  // 這裡產生新的檔名；透過原檔名取得副檔名再搭配日期跟亂數產生新檔名
-  $tmp = explode('.', $_FILES['img']['name']);
-  // 取得陣列最後一個元素
-  $subname = end($tmp);
-  $newFileName = date("YmdHis") . rand(10000, 99999) . "." . $subname;
-  move_uploaded_file($_FILES['img']['tmp_name'], "../imgs/" . $newFileName);
-
-  /*
-      application/vnd.openxmlformats-officedocument.wordprocessingml.document - word
-      application/vnd.openxmlformats-officedocument.spreadsheetml.sheet - excel
-      application/vnd.openxmlformats-officedocument.presentationml.presentation - ppt
-      application/pdf - pdf
-  */
 
   // 這裡在簡化檔案類型的名稱在資料表中所顯示過長的問題
   switch ($_FILES['img']['type']) {
@@ -55,15 +43,45 @@ if (!empty($_FILES['img']['tmp_name'])) {
       $type = 'other';
   }
 
-  $file = [
-    'name' => $newFileName,
-    'type' => $type,
-    'size' => $_FILES['img']['size'],
-    'desc' => $_POST['desc']
-  ];
+  // 這裡處理重新上傳的檔案的類型跟原檔案不一樣時的邏輯
+  if ($type != $file['type']) {
+    // 這裡更新資料表的 type 欄位
+    $file['type'] = $type;
 
-  insert('files', $file);
-  header('location:../manage.php');
-} else {
-  header('location:../edit_file.php?err=上傳失敗');
+    // 這裡對真實檔案的副檔名進行更新
+    $subname = end(explode(".", $_FILES['img']['name'])); // 這裡取得新檔案的副檔名
+    $tmp = explode(".", $file['name']); // 這裡將原檔案的檔名拆成數個陣列元素
+    $tmp[count($tmp) - 1] = $subname; // 這裡用新檔案的副檔名覆蓋掉原始檔案的
+    $file['name'] = join(".", $tmp);
+  }
+
+  // $file = [
+  //   'name' => $newFileName,
+  //   'type' => $type,
+  //   'size' => $_FILES['img']['size'],
+  //   'desc' => $_POST['desc']
+  // ];
+
+  $file['type'] = $type;
+  $file['size'] = $_FILES['img']['size'];
+} else { // 這裡要處理使用者沒有上傳新的檔案，只想更改檔名的邏輯
+  if ($_POST['name'] != $file['name']) {
+
+
+    // 這行則要更改真實檔案的檔名，使用內建函數 rename() : 兩個參數都要是包含檔名的完整路徑
+    rename("../imgs/" . $file['name'], "../imgs/" . $_POST['name']);
+
+    // 這行只有更改資料表中的 name 欄位值；注意這行一定要在 rename 完之後
+    $file['name'] = $_POST['name'];
+  }
 }
+
+// 這裡判斷表單送來的 desc 和資料表中的 desc 欄位是否不同，不同代表需要更新 desc 
+if ($_POST['desc'] != $file['desc']) {
+  $file['desc'] = $_POST['desc'];
+}
+
+update('files', $_POST['id'], $file);
+header('location:../manage.php');
+
+// header('location:../edit_file.php?err=上傳失敗');
--- edit_file.php
+++ edit_file.php
@@ -14,8 +14,7 @@ if (isset($_GET['id'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>編輯檔案</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
 
   <link rel="stylesheet" href="style.css">
@@ -77,6 +76,7 @@ if (isset($_GET['id'])) {
         </tr>
       </table>
       <div class=" text-center m-3">
+        <input type="hidden" name="id" value="<?= $file['id']; ?>">
         <input type="submit" value="更新">
       </div>
   </form>
@@ -91,9 +91,8 @@ if (isset($_GET['id'])) {
 
 
   ?>
-  <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+  <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+  </script>
 
 </body>
 
