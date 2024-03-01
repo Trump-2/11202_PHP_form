c859cf95c4dfb13a46c4550a6aa48f7c16357c02
YIHONG <akashidaiki2@gmail.com>
2024-03-01 11:33:53 +0800

    將從資料庫讀出的資料表資料以csv格式匯出下載

--- teacher_version/text-export.php
+++ teacher_version/text-export.php
@@ -3,28 +3,51 @@ include "db_export.php";
 
 
 if (!empty($_POST)) {
-    echo "您希望匯出";
-    print_r($_POST['select']);
-    echo "這些資料";
+    $rows = all("20200706", " where `投票所編號` in ('" . join("','", $_POST['select']) . "')");
+    // print_r($rows);
+
+    $filename = date("Ymd") . rand(100000000, 999999999);
+    $file = fopen("./doc/{$filename}.csv", "w+"); // w+ 是內建參數
+
+    // 加入 utf-8 編碼的 BOM
+    fwrite($file, "\xEF\xBB\xBF");
+
+
+    // 預設為 false
+    $chk = false;
+    foreach ($rows as $row) {
+        // 這個判斷式要取得表單最上面的欄位名稱那列
+        if (!$chk) {
+            $cols = array_keys($row);
+            fwrite($file, join(',', $cols) . "\r\n");
+            $chk = true;
+        }
+        fwrite($file, join(',', $row) . "\r\n"); // \r\n 代表換行，適用於 windows 系統；\n 也代表換行，用於 unix、linux 系統
+    }
+
+    fclose($file);
+
+    // download 參數:讓 href 連結到的檔案變成可下載
+    echo "<a href='./doc/{$filename}.csv' download>檔案已匯出，請點此連結下載</a>";
 }
 ?>
 
 <style>
-table {
-    border-collapse: collapse;
-}
+    table {
+        border-collapse: collapse;
+    }
 
-td {
-    border: 1px solid #666;
-    padding: 5px 12px;
-}
+    td {
+        border: 1px solid #666;
+        padding: 5px 12px;
+    }
 
-th {
-    border: 1px solid #666;
-    padding: 5px 12px;
-    background: #000;
-    color: #fff;
-}
+    th {
+        border: 1px solid #666;
+        padding: 5px 12px;
+        background: #000;
+        color: #fff;
+    }
 </style>
 
 <form action="?" method="post">
